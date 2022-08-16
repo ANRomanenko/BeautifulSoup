ea81d959fc9529dc124ff0069431ec1fe8e25693
romanenko <lifeapricot@gmail.com>
2022-08-16 20:08:01 +0300

    new files

--- tviy_clone_3.py
+++ tviy_clone_3.py
@@ -4,9 +4,5 @@ from bs4 import BeautifulSoup
 url = 'https://allo.ua/ru/processory/'
 
 response = requests.get(url)
-print(response)
-
 soup = BeautifulSoup(response.text, 'lxml')
-cards = []
-for item in soup.find_all():
-    print(item)
+print(soup)
